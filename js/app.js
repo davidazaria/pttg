56f516fe7f399f5450df1bbdf210e7cdb71c57b1
= <azaria.david@gmail.com>
2017-11-13 17:50:42 -0500

    added new feature to block the submit button on form field unless user enters in information in all three rows.

--- js/app.js
+++ js/app.js
@@ -6,9 +6,10 @@ let playerName = null;
 let playerTwitter = null;
 let playerZip = null;
 
-//  the below click function will start the game
-//  what this does in essence is showing certain section classes while hiding others
-//  so for each click based on a button's id, it will hide/show certain sections
+//  the below click function will hide all the section classes that are not yet in action until, if ever, we get to it based on the corresponding click events
+//  this was my low-brow way of handling multiple html pages by showing/hiding different section classes. this was excellent practice for click event "toggles"
+// line 27 was originally a "won't do" feature that i decided to tackle to give me some more jQuery beefiness in what was an otherwise straightforward game
+// what line 27 does is disable my submit button unless i had entered information in all three of my form fields. not a bad feature all things considered.
 
 $(() => {
   console.log('Ready for Trump Tweets!');
@@ -22,9 +23,15 @@ $(() => {
     console.log('Welcome to the form page!');
     $('.homepage').hide();
     $('.formpage').show();
-  });
 
-  //  on this click function, we will be storing the user's name, twitter handle, and zip code into the DOM, and below will we console logging it to prove the user entry into the form worked!
+    if ($('.row').val() === '') { $('#btontwo').attr('disabled', true); }
+    $('.row').keyup(() => {
+      if (($('#name').val() !== '') &&
+           ($('#twitterhandle').val() !== '') &&
+           ($('#zip_code').val() !== '')
+      ) { $('#btontwo').attr('disabled', false); } else { $('#btontwo').attr('disabled', true); }
+    });
+  });
 
   $('#btontwo').click(() => {
     console.log('Welcome to the intructions page!');
@@ -69,15 +76,16 @@ $(() => {
   //  starting at 0, which I will increment up for every click, and then kill the game once I've hit the 10th click
   let clickCount = 0;
 
-
   //  in this click event on the class of game button, which represents both options the user could click to log a response, I will be grabbing and manipulating a series of information
   //  for one, I will be grabbing the id of the button to match against the id of the tweet for logging purposes
 
   $('.game_bton').click((e) => {
     clickArray.push(e.target.id);
     clickCount++;
+
     //  in addition, given the number of clicks I am on, I will also trigger the eventual winning event to finalize the game. ideally i would like to move this function elsewhere along with the click(gamePlay) functionality
-    if (clickCount === 9) {
+
+    if (clickCount === 10) {
       console.log('Thanks for playing!');
       $('.gamepage').hide();
       $('.finalpage').show();
@@ -89,24 +97,25 @@ $(() => {
   function gamePlay(event) {
     if ('url' in tweetArray[tweetCounter]) { //  guards against going too far in the number of clicks so that there is no log of a click against an item in an array that does not exist
       $('#tweet').attr('src', tweetArray[tweetCounter].url); // swaps the image of a tweet based on the tweetCounter position
+
       $('.progress').text(`question ${tweetCounter + 1} ` + `out of ${tweetArray.length}`);
       tweetCounter++; // adds to the DOM the current position of the game the user is in
 
       //  within the same function bloated function in need of a refactoring, as i loop through the length of my array, i stash the matches of the id's of the tweet and click's id
       //  if there is a match, the we increment the variable for score with a point
 
-      let score = 0;
+      let score = 0; // here i start with a blank score which i will continuously add to through the below for loop
       for (let i = 0; i < tweetArray.length; i++) {
         if (tweetArray[i].id === clickArray[i]) {
           score++;
         }
       }
       $('.score').text(`current score is: ${score}`); //  additionally, we throw the current score on the game page
-
       $('#scored').text(`your score was ${score} out of ${tweetArray.length}! thanks for playing, ${playerName}!`); //  and finally we know to update the final page with the score's final tally
     }
   }
-  //  and here is where we are calling the above function to trigger the aforementioned actions based on the button id found here
-  $('#real').click(gamePlay);
-  $('#fake').click(gamePlay);
+
+  $('#real').click(gamePlay); //  this is to call the gamePlay function
+  $('#fake').click(gamePlay); //  this is to call the gamePlay function
+
 });
